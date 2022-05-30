a135eca8d31f098fd097b909d3e77b8482f63d85
dong-Hyeok <mimiga1@naver.com>
2022-05-31 00:18:31 +0900

    20220531-11

--- app/src/main/java/org/techtown/today/add_long.java
+++ app/src/main/java/org/techtown/today/add_long.java
@@ -2,7 +2,6 @@ package org.techtown.today;
 
 import android.app.DatePickerDialog;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -20,6 +19,14 @@ public class add_long extends Fragment {
     Button enddate_btn;
     Calendar myCalendar;
 
+    Button sun_btn;
+    Button mon_btn;
+    Button tue_btn;
+    Button wed_btn;
+    Button thu_btn;
+    Button fri_btn;
+    Button sat_btn;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -27,6 +34,14 @@ public class add_long extends Fragment {
 
         startdate_btn = rootView.findViewById(R.id.startdate_btn);
         enddate_btn = rootView.findViewById(R.id.enddate_btn);
+        sun_btn = rootView.findViewById(R.id.sun_btn);
+        mon_btn = rootView.findViewById(R.id.mon_btn);
+        tue_btn = rootView.findViewById(R.id.tue_btn);
+        wed_btn = rootView.findViewById(R.id.wed_btn);
+        thu_btn = rootView.findViewById(R.id.thu_btn);
+        fri_btn = rootView.findViewById(R.id.fri_btn);
+        sat_btn = rootView.findViewById(R.id.sat_btn);
+
 
         myCalendar = Calendar.getInstance();
         DatePickerDialog.OnDateSetListener startDatePicker = new DatePickerDialog.OnDateSetListener() {
@@ -62,6 +77,96 @@ public class add_long extends Fragment {
                 new DatePickerDialog(getContext(), endDatePicker, myCalendar.get(Calendar.YEAR), myCalendar.get(Calendar.MONTH), myCalendar.get(Calendar.DAY_OF_MONTH)).show();
              }
         });
+
+
+        sun_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(sun_btn.isSelected() == true){
+                    sun_btn.setSelected(false);
+                }
+                else{
+                    sun_btn.setSelected(true);
+                }
+
+            }
+        });
+
+        mon_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(mon_btn.isSelected() == true){
+                    mon_btn.setSelected(false);
+                }
+                else{
+                    mon_btn.setSelected(true);
+                }
+
+            }
+        });
+
+        tue_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(tue_btn.isSelected() == true){
+                    tue_btn.setSelected(false);
+                }
+                else{
+                    tue_btn.setSelected(true);
+                }
+
+            }
+        });
+
+        wed_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(wed_btn.isSelected() == true){
+                    wed_btn.setSelected(false);
+                }
+                else{
+                    wed_btn.setSelected(true);
+                }
+            }
+        });
+
+        thu_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(thu_btn.isSelected() == true){
+                    thu_btn.setSelected(false);
+                }
+                else{
+                    thu_btn.setSelected(true);
+                }
+            }
+        });
+
+        fri_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(fri_btn.isSelected() == true){
+                    fri_btn.setSelected(false);
+                }
+                else{
+                    fri_btn.setSelected(true);
+                }
+            }
+        });
+
+        sat_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(sat_btn.isSelected() == true){
+                    sat_btn.setSelected(false);
+                }
+                else{
+                    sat_btn.setSelected(true);
+                }
+            }
+        });
+
+
         return rootView;
     }
     private void updateLabel(String str) {
